840babd1ed0dbaa2e1ea01b8051baf691b18a256
Elsayed-Elpna <said.elpna@yahoo.com>
2026-06-13 13:41:06 +0300

    finsh setup models for project and env setp up for posgresql

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from store.models import Product
 # Create your views here.
 def say_hello(request):
-  x = 1
-  y = 2
-  return HttpResponse("hello")
+  query_set = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
+  for pro in query_set:
+    print(pro)
+  return render(request,'hello.html') 
--- storefront/settings.py
+++ storefront/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(#&c280lip5__yu)c)!o=+3a1072uefgib&7h3zqu_$f#jnp(*'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -40,7 +41,8 @@ INSTALLED_APPS = [
     "debug_toolbar",
     'playground',
     'tags',
-    'store'
+    'store',
+    'likes'
 ]
 
 MIDDLEWARE = [
@@ -86,8 +88,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
